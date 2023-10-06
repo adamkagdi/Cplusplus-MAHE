7eae47470c38e373b799c2aab6b63e4b78e08f3f
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2023-10-06 11:36:20 +0400

    Adding files

---
+++ Hello-world/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "My name is Adam" << endl;
+    cout << "I study at MAHE Dubai" << endl;
+    return 0;
+}
