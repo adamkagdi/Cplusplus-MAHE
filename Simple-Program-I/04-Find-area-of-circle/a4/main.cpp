75bca35e1bb822f366de03b3069a5e2d41fc3c25
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2023-10-18 21:14:50 +0400

    more work done

---
+++ Simple-Program-I/04-Find-area-of-circle/a4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    float pi = 3.14;
+    cout << "Enter Radius of the Circle" << endl;
+    cin >> r;
+    cout << r*r*pi;
+    return 0;
+}
