823b8bb866fa3d176ba6b9dd36477f5188e981cc
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2023-10-18 20:14:02 +0400

    Assignment completion

---
+++ Simple-Program-I/01-Print-statement-on-screen/q1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "My name is Adam Kagdi" << endl;
+    cout << "I am doing B.Tech Computer Science Engineering" << endl;
+    return 0;
+}
---
+++ Simple-Program-I/02-Perform-arithmetic-operations/a2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "enter value a\n" ;
+    cin >> a ;
+    cout << "enter value b\n" ;
+    cin >> b ;
+    cout << a + b ;
+    cout << "\n" ;
+    cout << a - b ;
+    cout << "\n" ;
+    cout << a * b ;
+    cout << "\n" ;
+    cout << a / b ;
+    return 0;
+}
