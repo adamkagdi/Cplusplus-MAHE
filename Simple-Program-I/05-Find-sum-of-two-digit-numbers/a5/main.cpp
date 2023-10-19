ed219542aa6b3903efb79a2946da7385a92230ba
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2023-10-19 18:44:38 +0400

    Completed 5th Question

---
+++ Simple-Program-I/05-Find-sum-of-two-digit-numbers/a5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+    cout << "What should be 'a'?" << endl;
+    cin >> a;
+    cout << "What should be 'b'?" << endl;
+    cin >> b;
+    c = a + b;
+    cout << "The Sum of both is " << c << endl;
+    return 0;
+}
