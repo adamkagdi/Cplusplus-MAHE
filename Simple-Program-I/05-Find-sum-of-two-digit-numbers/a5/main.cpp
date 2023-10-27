51d88ad52c9f2ae67530e0d61f0ce20f74b56a6b
Adam Kagdi <71578844+adamkagdi@users.noreply.github.com>
2023-10-27 11:42:59 +0400

    Updates more

--- Simple-Program-I/05-Find-sum-of-two-digit-numbers/a5/main.cpp
+++ Simple-Program-I/05-Find-sum-of-two-digit-numbers/a5/main.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main ()
 {
-    float n,sum,m;
+    int x,sum,y;
     sum = 0;
     cout << "Enter a 2 digit number \n";
-    cin >> n;
-
+    cin >> x;
+    while (x>0)
+    {
+        y=x%10;
+        sum=sum+y;
+        x=x/10;
+    }
+    cout << "Sum of the number is " << sum;
+    return 0;
 }
